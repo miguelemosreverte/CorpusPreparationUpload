e6314e271afd9a5cf7c19c6da5e7c9897bf3e458
miguelemosreverte <miguellemos95@gmail.com>
2017-05-27 23:17:05 -0300

    Now files are correctly saved inside a folder by the name of the provided Language Model name, or LM_name

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -9,6 +9,7 @@ import javax.inject.Singleton;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * This class uses a custom body parser to change the upload type.
@@ -39,8 +40,44 @@ public class HomeController extends Controller {
         final File CorpusPreparationSource = CorpusPreparationSource_filePart.getFile();
         final File CorpusPreparationTarget = CorpusPreparationTarget_filePart.getFile();
         final File CorpusPreparationLM = CorpusPreparationLM_filePart.getFile();
-        System.out.println(LM_name);
+
+        String final_dir = "./"+LM_name+"/";
+
+        File theDir = new File(final_dir);
+        if (!theDir.exists()) {
+            theDir.mkdir();
+        }
+
+        String CorpusPreparationSource_filePath = final_dir+"CorpusPreparationSource";
+        String CorpusPreparationTarget_filePath = final_dir+"CorpusPreparationTarget";
+        String CorpusPreparationLM_filePath = final_dir+"CorpusPreparationLM";
+        Files.deleteIfExists(Paths.get(CorpusPreparationSource_filePath));
+        Files.deleteIfExists(Paths.get(CorpusPreparationTarget_filePath));
+        Files.deleteIfExists(Paths.get(CorpusPreparationLM_filePath));
+
+        CorpusPreparationSource.renameTo(new File(CorpusPreparationSource_filePath));
+        CorpusPreparationTarget.renameTo(new File(CorpusPreparationTarget_filePath));
+        CorpusPreparationLM.renameTo(new File(CorpusPreparationLM_filePath));
+
+
         return ok("Success!");
+    }
+
+
+    private long operateOnTempFile(String final_dir, String final_name, File file) throws IOException {
+        System.out.println(final_name);
+        final long size = Files.size(file.toPath());
+        Files.deleteIfExists(Paths.get(final_dir+final_name));
+        File theDir = new File(final_dir);
+        if (!theDir.exists()) {
+            theDir.mkdir();
+        }
+        if(file.renameTo(new File(final_dir + final_name))){
+            System.out.println("File is moved successful!");
+        }else{
+            System.out.println("File is failed to move!" + final_dir + file.getName() );
+        }
+        return size;
     }
 }
 
